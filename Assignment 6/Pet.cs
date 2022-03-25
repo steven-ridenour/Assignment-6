a5a284271b02070d685e357bd0dac7569036784d
riden <riden@STEVENS-DESKTOP>
2022-03-25 08:21:42 -0400

    Add project files.

---
+++ Assignment 6/Pet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6
+{
+    public class Pet
+    {
+        public int id;
+        public string name;
+        public string breed;
+        public int age;
+
+    }
+}
